45f84a3f08fe72246ac921fe421bb63993593289
Juliano-Mac <julianobsg@gmail.com>
2014-05-26 19:30:31 -0300

    if

---
+++ src/br/edu/compilador/ComandoIf.java
@@ -0,0 +1,20 @@
+package br.edu.compilador;
+
+public class ComandoIf extends Comando{
+	public Object exprL;
+	public Object exprR;
+	public int operator;
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String toC() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
