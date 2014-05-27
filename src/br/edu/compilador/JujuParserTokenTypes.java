b1e28754cd34ab241cb55a2e07030e72c52f8085
thaisrt <thaisrtoledo@gmail.com>
2014-05-27 14:07:24 -0300

    no message

---
+++ src/br/edu/compilador/JujuParserTokenTypes.java
@@ -0,0 +1,37 @@
+// $ANTLR 2.7.6 (2005-12-22): "gramatica.g" -> "JujuParser.java"$
+
+public interface JujuParserTokenTypes {
+	int EOF = 1;
+	int NULL_TREE_LOOKAHEAD = 3;
+	int LITERAL_function = 4;
+	int T_id = 5;
+	int LITERAL_end = 6;
+	int T_tipo = 7;
+	int T_pv = 8;
+	int T_atrib = 9;
+	int T_msg = 10;
+	int LITERAL_if = 11;
+	int LITERAL_then = 12;
+	int LITERAL_else = 13;
+	int LITERAL_begin = 14;
+	int T_num = 15;
+	int LITERAL_while = 16;
+	int T_or = 17;
+	int T_and = 18;
+	int T_eq = 19;
+	int T_neq = 20;
+	int T_gt = 21;
+	int T_lt = 22;
+	int T_plus = 23;
+	int T_minus = 24;
+	int T_times = 25;
+	int T_div = 26;
+	int LITERAL_input = 27;
+	int T_ap = 28;
+	int T_fp = 29;
+	int LITERAL_output = 30;
+	int T_vir = 31;
+	int T_gteq = 32;
+	int T_lteq = 33;
+	int T_ws = 34;
+}
